b42a6dc6c147ba9f1fb80376618efc9bc47ec1ee
mihirdeshpande <mihirdeshpande@users.noreply.github.com>
2023-08-04 19:38:00 +0000

    Added timer for setup

--- server_code/options/get_strategies.py
+++ server_code/options/get_strategies.py
@@ -1,5 +1,6 @@
 import copy
 import concurrent.futures
+import time
 
 import pandas as pd
 import yfinance as yf
@@ -12,97 +13,108 @@ from server_code.options.utils import add_daily_moves
 
 
 class OptionStrategies:
-    def __init__(self, budget):
-        self.budget = budget
-        self.__symbols_info_df = None
-        self.all_strategies = list()
 
-    @staticmethod
-    def get_basic_info_row(symbol_ticker):
-        return [symbol_ticker.info.get(key, None) for key in BASIC_INFO_COLUMNS]
+  def __init__(self, budget):
+    self.budget = budget
+    self.__symbols_info_df = None
+    self.all_strategies = list()
 
-    @staticmethod
-    def are_weekly_options_available(symbol_ticker):
-        options = symbol_ticker.options
-        if not options or len(options) < 2:
-            return False
-        options_1_date = datetime.strptime(options[0], '%Y-%m-%d')
-        options_2_date = datetime.strptime(options[1], '%Y-%m-%d')
-        if (options_2_date - options_1_date).days > 7:
-            return False
-        return True
+  @staticmethod
+  def get_basic_info_row(symbol_ticker):
+    return [symbol_ticker.info.get(key, None) for key in BASIC_INFO_COLUMNS]
 
-    def __populate_symbols_info(self):
-        tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
-        self.tickers_info = tickers_object.tickers   # tickers_info: {symbol: yfinance.Ticker}
-        rows = []
-        for symbol, symbol_ticker in self.tickers_info.items():
-            try:
-                if self.are_weekly_options_available(symbol_ticker):
-                    new_row = self.get_basic_info_row(symbol_ticker)
-                    rows.append(new_row)
-            except Exception as ex:
-                pass
-        self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
-        x = 1
+  @staticmethod
+  def are_weekly_options_available(symbol_ticker):
+    options = symbol_ticker.options
+    if not options or len(options) < 2:
+      return False
+    options_1_date = datetime.strptime(options[0], '%Y-%m-%d')
+    options_2_date = datetime.strptime(options[1], '%Y-%m-%d')
+    if (options_2_date - options_1_date).days > 7:
+      return False
+    return True
 
-    def __get_ticker_data(self, symbol, symbol_ticker):
-        try:
-            if self.are_weekly_options_available(symbol_ticker):
-                return self.get_basic_info_row(symbol_ticker)
-        except Exception as ex:
-            return None
+  def __populate_symbols_info(self):
+    tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
+    self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
+    rows = []
+    for symbol, symbol_ticker in self.tickers_info.items():
+      try:
+        if self.are_weekly_options_available(symbol_ticker):
+          new_row = self.get_basic_info_row(symbol_ticker)
+          rows.append(new_row)
+      except Exception as ex:
+        pass
+    self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
+    x = 1
 
-    def __populate_symbols_info_threaded(self):
-        tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
-        self.tickers_info = tickers_object.tickers   # tickers_info: {symbol: yfinance.Ticker}
-        rows = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            futures = [executor.submit(self.__get_ticker_data, symbol, symbol_ticker) for symbol, symbol_ticker in self.tickers_info.items()]
+  def __get_ticker_data(self, symbol, symbol_ticker):
+    try:
+      if self.are_weekly_options_available(symbol_ticker):
+        return self.get_basic_info_row(symbol_ticker)
+    except Exception as ex:
+      return None
 
-            # Wait for all futures to complete
-            concurrent.futures.wait(futures)
+  def __populate_symbols_info_threaded(self):
+    tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
+    self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
+    rows = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+      futures = [
+        executor.submit(self.__get_ticker_data, symbol, symbol_ticker)
+        for symbol, symbol_ticker in self.tickers_info.items()
+      ]
 
-            # Retrieve the results from the futures (ticker data or None for failed requests)
-            rows = [future.result() for future in futures]
+      # Wait for all futures to complete
+      concurrent.futures.wait(futures)
 
-        self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
+      # Retrieve the results from the futures (ticker data or None for failed requests)
+      rows = [future.result() for future in futures]
 
-    @property
-    def symbols_info(self):
-        if self.__symbols_info_df is None:
-            self.__populate_symbols_info_threaded()
-        return self.__symbols_info_df
+    self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
 
-    def get_put_credit_spreads(self):
-        put_strategy = PutCreditStrategy(copy.deepcopy(self.__symbols_info_df), self.budget, self.tickers_info)
-        put_strategies = put_strategy.get_put_credit_strategies()
-        self.all_strategies.extend(put_strategies)
+  @property
+  def symbols_info(self):
+    if self.__symbols_info_df is None:
+      self.__populate_symbols_info_threaded()
+    return self.__symbols_info_df
 
-    def clean_info_df(self):
-        self.__symbols_info_df = self.__symbols_info_df.dropna(subset=['currentPrice'])
+  def get_put_credit_spreads(self):
+    put_strategy = PutCreditStrategy(copy.deepcopy(self.__symbols_info_df),
+                                     self.budget, self.tickers_info)
+    put_strategies = put_strategy.get_put_credit_strategies()
+    self.all_strategies.extend(put_strategies)
 
-    def process_info_df(self):
-        self.clean_info_df()
-        add_daily_moves(self.__symbols_info_df)
+  def clean_info_df(self):
+    self.__symbols_info_df = self.__symbols_info_df.dropna(
+      subset=['currentPrice'])
 
-    def setup(self):
-        # self.__populate_symbols_info()
-        self.__populate_symbols_info_threaded()
-        self.process_info_df()
+  def process_info_df(self):
+    self.clean_info_df()
+    add_daily_moves(self.__symbols_info_df)
+
+  def setup(self):
+    # self.__populate_symbols_info()
+    self.__populate_symbols_info_threaded()
+    self.process_info_df()
 
 
 def get_strategies(budget):
-    print("Starting...")
-    option_strategies = OptionStrategies(budget=budget)
-    option_strategies.setup()
-    print("Setup Done...")
-    option_strategies.get_put_credit_spreads()
-    print("Below generated: ")
-    # print(option_strategies.all_strategies)
-    return option_strategies.all_strategies
+  print("Starting 10 thread...")
+  option_strategies = OptionStrategies(budget=budget)
+
+  start_time = time.time()
+  option_strategies.setup()
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  print(f"Elapsed Time: {elapsed_time:.6f} seconds")
+  print("Setup Done...")
+  option_strategies.get_put_credit_spreads()
+  print("Below generated: ")
+  # print(option_strategies.all_strategies)
+  return option_strategies.all_strategies
 
 
 if __name__ == '__main__':
-    strategies = get_strategies(budget=1000)
-    x = 1
+  strategies = get_strategies(budget=1000)
+  x = 1
