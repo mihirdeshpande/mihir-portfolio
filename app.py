f47d6f369ab394f65ef83957279e41374a3ce724
mihirdeshpande <mihirdeshpande91@gmail.com>
2023-08-08 20:09:50 -0400

    Added multithreading for calling strategies. Code reformat

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import datetime
-import pytz
-
 from flask import Flask, render_template, request
+
 from server_code.options.get_strategies import get_strategies
 from server_code.utils.utils import get_est_now
 
@@ -10,25 +8,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_home():
-  return render_template('home.html')
+    return render_template('home.html')
 
 
 @app.route("/options/")
 def render_options():
-  return render_template('options_suggestor.html')
+    return render_template('options_suggestor.html')
 
 
 @app.route("/options/get_strategies", methods=['GET'])
 def fetch_strategies():
-  budget = int(request.args.get('budget'))
-  strategies = get_strategies(budget)
-  if strategies:
-    return render_template(
-      'options_strategies_results.html',
-      strategies=strategies,
-      generated_at=get_est_now().strftime("%m/%d/%Y %I:%M:%S %p"))
-  return render_template('options_no_results.html')
+    budget = int(request.args.get('budget'))
+    strategies = get_strategies(budget)
+    if strategies:
+        return render_template(
+            'options_strategies_results.html',
+            strategies=strategies,
+            generated_at=get_est_now().strftime("%m/%d/%Y %I:%M:%S %p"))
+    return render_template('options_no_results.html')
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- server_code/options/get_strategies.py
+++ server_code/options/get_strategies.py
@@ -1,129 +1,136 @@
-import copy
 import concurrent.futures
+import copy
 import time
+from datetime import datetime
 
 import pandas as pd
 import yfinance as yf
 
-from datetime import datetime
-from server_code.options.most_traded_symbols import MOST_TRADED_SYMBOLS
-from server_code.options.consts import BASIC_INFO_COLUMNS
-from server_code.options.put_credit_spread import PutCreditStrategy
 from server_code.options.call_credit_spread import CallCreditStrategy
+from server_code.options.consts import BASIC_INFO_COLUMNS
+from server_code.options.most_traded_symbols import MOST_TRADED_SYMBOLS
+from server_code.options.put_credit_spread import PutCreditStrategy
 from server_code.options.utils import add_daily_moves
 
 
 class OptionStrategies:
 
-  def __init__(self, budget):
-    self.budget = budget
-    self.__symbols_info_df = None
-    self.all_strategies = list()
+    def __init__(self, budget):
+        self.budget = budget
+        self.__symbols_info_df = None
+        self.all_strategies = list()
 
-  @staticmethod
-  def get_basic_info_row(symbol_ticker):
-    return [symbol_ticker.info.get(key, None) for key in BASIC_INFO_COLUMNS]
+    @staticmethod
+    def get_basic_info_row(symbol_ticker):
+        return [symbol_ticker.info.get(key, None) for key in BASIC_INFO_COLUMNS]
 
-  @staticmethod
-  def are_weekly_options_available(symbol_ticker):
-    options = symbol_ticker.options
-    if not options or len(options) < 2:
-      return False
-    options_1_date = datetime.strptime(options[0], '%Y-%m-%d')
-    options_2_date = datetime.strptime(options[1], '%Y-%m-%d')
-    if (options_2_date - options_1_date).days > 7:
-      return False
-    return True
+    @staticmethod
+    def are_weekly_options_available(symbol_ticker):
+        options = symbol_ticker.options
+        if not options or len(options) < 2:
+            return False
+        options_1_date = datetime.strptime(options[0], '%Y-%m-%d')
+        options_2_date = datetime.strptime(options[1], '%Y-%m-%d')
+        if (options_2_date - options_1_date).days > 7:
+            return False
+        return True
 
-  def __populate_symbols_info(self):
-    tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
-    self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
-    rows = []
-    for symbol, symbol_ticker in self.tickers_info.items():
-      try:
-        if self.are_weekly_options_available(symbol_ticker):
-          new_row = self.get_basic_info_row(symbol_ticker)
-          rows.append(new_row)
-      except Exception as ex:
-        pass
-    self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
-    x = 1
+    def __populate_symbols_info(self):
+        tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
+        self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
+        rows = []
+        for symbol, symbol_ticker in self.tickers_info.items():
+            try:
+                if self.are_weekly_options_available(symbol_ticker):
+                    new_row = self.get_basic_info_row(symbol_ticker)
+                    rows.append(new_row)
+            except Exception as ex:
+                pass
+        self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
+        x = 1
 
-  def __get_ticker_data(self, symbol, symbol_ticker):
-    try:
-      if self.are_weekly_options_available(symbol_ticker):
-        return self.get_basic_info_row(symbol_ticker)
-    except Exception as ex:
-      return None
+    def __get_ticker_data(self, symbol, symbol_ticker):
+        try:
+            if self.are_weekly_options_available(symbol_ticker):
+                return self.get_basic_info_row(symbol_ticker)
+        except Exception as ex:
+            return None
 
-  def __populate_symbols_info_threaded(self):
-    tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
-    self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
-    rows = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-      futures = [
-        executor.submit(self.__get_ticker_data, symbol, symbol_ticker)
-        for symbol, symbol_ticker in self.tickers_info.items()
-      ]
+    def __populate_symbols_info_threaded(self):
+        tickers_object = yf.Tickers(MOST_TRADED_SYMBOLS)
+        self.tickers_info = tickers_object.tickers  # tickers_info: {symbol: yfinance.Ticker}
+        rows = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [
+                executor.submit(self.__get_ticker_data, symbol, symbol_ticker)
+                for symbol, symbol_ticker in self.tickers_info.items()
+            ]
 
-      # Wait for all futures to complete
-      concurrent.futures.wait(futures)
+            # Wait for all futures to complete
+            concurrent.futures.wait(futures)
 
-      # Retrieve the results from the futures (ticker data or None for failed requests)
-      rows = [future.result() for future in futures]
+            # Retrieve the results from the futures (ticker data or None for failed requests)
+            rows = [future.result() for future in futures]
 
-    self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
+        self.__symbols_info_df = pd.DataFrame(rows, columns=BASIC_INFO_COLUMNS)
 
-  @property
-  def symbols_info(self):
-    if self.__symbols_info_df is None:
-      self.__populate_symbols_info_threaded()
-    return self.__symbols_info_df
+    @property
+    def symbols_info(self):
+        if self.__symbols_info_df is None:
+            self.__populate_symbols_info_threaded()
+        return self.__symbols_info_df
 
-  def get_put_credit_spreads(self):
-    put_strategy = PutCreditStrategy(copy.deepcopy(self.__symbols_info_df),
-                                     self.budget, self.tickers_info)
-    put_strategies = put_strategy.get_put_credit_strategies()
-    self.all_strategies.extend(put_strategies)
+    def get_put_credit_spreads(self):
+        put_strategy = PutCreditStrategy(copy.deepcopy(self.__symbols_info_df),
+                                         self.budget, self.tickers_info)
+        put_strategies = put_strategy.get_put_credit_strategies()
+        # self.all_strategies.extend(put_strategies)
+        return put_strategies
 
-  def get_call_credit_spreads(self):
-    call_strategy = CallCreditStrategy(copy.deepcopy(self.__symbols_info_df),
-                                     self.budget, self.tickers_info)
-    call_strategies = call_strategy.get_call_credit_strategies()
-    self.all_strategies.extend(call_strategies)
+    def get_call_credit_spreads(self):
+        call_strategy = CallCreditStrategy(copy.deepcopy(self.__symbols_info_df),
+                                           self.budget, self.tickers_info)
+        call_strategies = call_strategy.get_call_credit_strategies()
+        # self.all_strategies.extend(call_strategies)
+        return call_strategies
 
-  def clean_info_df(self):
-    self.__symbols_info_df = self.__symbols_info_df.dropna(
-      subset=['currentPrice'])
+    def clean_info_df(self):
+        self.__symbols_info_df = self.__symbols_info_df.dropna(
+            subset=['currentPrice'])
 
-  def process_info_df(self):
-    self.clean_info_df()
-    add_daily_moves(self.__symbols_info_df)
+    def process_info_df(self):
+        self.clean_info_df()
+        add_daily_moves(self.__symbols_info_df)
 
-  def setup(self):
-    # self.__populate_symbols_info()
-    self.__populate_symbols_info_threaded()
-    self.process_info_df()
+    def setup(self):
+        # self.__populate_symbols_info()
+        self.__populate_symbols_info_threaded()
+        self.process_info_df()
 
 
 def get_strategies(budget):
     print("Starting 10 thread...")
     option_strategies = OptionStrategies(budget=budget)
-
     start_time = time.time()
     option_strategies.setup()
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Elapsed Time: {elapsed_time:.6f} seconds")
-    print("Setup Done...")
-    option_strategies.get_put_credit_spreads()
-    option_strategies.get_call_credit_spreads()
-    print("Below generated: ")
-    # print(option_strategies.all_strategies)
+    print(f"Setup Done in: {elapsed_time:.6f} seconds")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        # Submit the functions to the executor
+        put_credit_spreads = executor.submit(option_strategies.get_put_credit_spreads)
+        call_credit_spreads = executor.submit(option_strategies.get_call_credit_spreads)
+
+        # Wait for both futures to complete
+        concurrent.futures.wait([put_credit_spreads, call_credit_spreads])
+
+    print("Strategies Generated!")
+    option_strategies.all_strategies.extend(put_credit_spreads.result() + call_credit_spreads.result())
     option_strategies.all_strategies.sort(key=lambda x: x['profit_to_risk'], reverse=True)
     return option_strategies.all_strategies
 
 
 if __name__ == '__main__':
-  strategies = get_strategies(budget=1000)
-  x = 1
+    strategies = get_strategies(budget=1000)
+    x = 1
